c5ce696826265813a1c0a005576fffa8a624d23b
Thomas <thomasfrank2008@hotmail.com>
2020-11-18 17:17:58 -0300

    arrumarcss

--- frontend/user/App.js
+++ frontend/user/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 
 // nossos imports
+import './global.css';
 import GlobalStyles from "./app/config/GlobalStyles";
 import HomeScreen from "./app/scenes/HomeScreen";
 import Index from "./app/registerU/index";
--- frontend/user/app/registerU/index.js
+++ frontend/user/app/registerU/index.js
@@ -31,16 +31,20 @@ export default function Register (){
     return(
         <div className="register-container">
             <div className="content">
-                <section>
-                    <h1>Cadastro</h1>
-
-                    <p>Cadastre agora mesmo, Para poder adotar animais.</p>
-                    <a className="back-link" to="/http:localhost:registerU">
-                        <FiArrowLeft size={16} color="#000000" />
-                        <p>Já tenho cadastro</p>
-                    </a>
-                </section>
+                
+                
                 <form onSubmit={handleRegister}>
+                    <section>
+                    <h1>Cadastro</h1>
+                    
+                    <img  src = "https://library.kissclipart.com/20191125/qoe/kissclipart-family-day-happy-family-day-family-fa3e1eccda3b196e.png"  width="100%" height="40%"/>
+                    <p>Cadastre agora mesmo, Para poder adotar animais.</p>
+                    
+                    
+                   
+                    </section>
+                    
+                    <div class="container-fluid">
                     <input placeholder="Nome do Usuario" value={nome_User} onChange={e => setNomeU(e.target.value)}/>
                     <input type="email" placeholder="Email" value={email_User} onChange={e => setEmailU(e.target.value)}/>
                     <input placeholder="WhatsApp" value={whatsApp_User} onChange={e => setWhatsAppU(e.target.value)}/>
@@ -50,8 +54,13 @@ export default function Register (){
                         <input placeholder="UF" style={{width:80}} value={uf_User} onChange={e => setUfU(e.target.value)}/>
                     </div>
                     <button className="button" type="submit">Cadastrar</button>
-
+                    <a className="back-link" to="/http:localhost:registerU">
+                        <FiArrowLeft size={16} color="#000000" />
+                        <p>Já tenho cadastro</p>
+                    </a>
+                    </div>
                 </form>
+                
             </div>
         </div>
     );
